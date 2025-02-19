632fd87b3e4d1fe9303d377b2c1844d2ba098874
cesaraugustooo <164028550+cesaraugustooo@users.noreply.github.com>
2025-02-19 15:47:57 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+
 async function getMateria(){
     try{
-        const api = await fetch('http://localhost/RevistaDigital_API/categorias');
+        const api = await fetch('http://localhost:8000/posts');
         const data = await api.json();
         console.log(data)
     }
@@ -8,5 +9,29 @@ async function getMateria(){
         console.error('Erro' ,error);
     }
 }
+async function inerMateria(){
+    try {
+        const api = await fetch('http://localhost:8000/posts');
+    
+        const data = await api.json();
+    
+        const publiSection = document.getElementById('publis'); 
+        publiSection.innerHTML = ''; 
+        data.forEach(post => {
+            publiSection.innerHTML += `
+                <div class="materias" style="max-height: 700px;"><div class="titulo"><h1 class="title">${post.titulo_post}</h1>
+                </div><div class="img"><img src="${post.foto_post}" alt=""></div><div class="conteudo"><p>${post.descricao_post}</p></div></div>`;
+            
+        });
+    
 
-getMateria();
+        console.log(data);
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+    
+}
+
+
+
+inerMateria();
