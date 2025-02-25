c367e350b35128fcab668c9ad7457482efebfa87
cesaraugustooo <164028550+cesaraugustooo@users.noreply.github.com>
2025-02-24 22:28:16 -0300

    Add files via upload

---
+++ js/painelDeControle.js
@@ -0,0 +1,64 @@
+
+async function postMateria(){
+    const titulo_post = document.getElementById('titulo_post').value;
+    const foto_post = document.getElementById('foto_post').value;
+    const descricao_post = document.getElementById('descricao_post').value;
+    const data_criacao_post = document.getElementById('data_criacao_post').value;
+    const usuarios_id_usuario = document.getElementById('usuarios_id_usuario').value;
+    const categorias_id_categoria = document.getElementById('categorias_id_categoria').value;
+
+
+    const dados ={
+        titulo_post: titulo_post,
+        foto_post: foto_post,
+        descricao_post: descricao_post,
+        data_criacao_post: data_criacao_post,
+        usuarios_id_usuario: usuarios_id_usuario,
+        categorias_id_categoria: categorias_id_categoria
+    }
+
+        try {
+            const response = await fetch('http://localhost/RevistaDigital_API/posts', {
+                method: 'POST',
+                headers:{'Content-Type': 'application/json',},
+                body: JSON.stringify(dados)
+        });
+
+            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-success">Categoria criada com sucesso!</div>`;
+            document.getElementById('form-categoria').reset(); // Limpa o formulário
+        } catch (error) {
+            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-danger">Erro ao criar categoria: ${error.message}</div>`;
+        }
+}
+
+
+
+async function postUsers(){
+    const user_usuario = document.getElementById('user_usuario').value;
+    const senha_usuario = document.getElementById('senha_usuario').value;
+    const nivel = document.getElementById('nivel').value;
+
+
+
+    const dados ={
+        user_usuario: user_usuario,
+        senha_usuario: senha_usuario,
+        nivel: nivel,
+       
+    }
+
+        try {
+            const response = await fetch('http://localhost/RevistaDigital_API/users', {
+                method: 'POST',
+                headers:{'Content-Type': 'application/json',},
+                body: JSON.stringify(dados)
+        });
+
+            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-success">Categoria criada com sucesso!</div>`;
+            document.getElementById('form-categoria').reset(); // Limpa o formulário
+        } catch (error) {
+            // Exibe mensagem de erro
+            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-danger">Erro ao criar categoria: ${error.message}</div>`;
+        }
+}
+
--- js/requests.js
+++ js/requests.js
@@ -1,4 +1,3 @@
-
 const BACKEND = 'http://localhost/RevistaDigital_API'
 
 async function get(ent){
@@ -7,121 +6,37 @@ async function get(ent){
         .then(data)
 }
 async function inerMateria(){
-    try {
-        const api = await fetch(`${BACKEND}/posts`);
+    try{
+     const api = await fetch(`${BACKEND}/posts`);
+     const data = await api.json();
     
-        const data = await api.json();
-    
-        const publiSection = document.getElementById('publis'); 
-        publiSection.innerHTML = ''; 
-        data.forEach(post => {
-            publiSection.innerHTML += `
-                <div class="materias" style="max-height: 700px;"><div class="titulo"><h1 class="title">${post.titulo_post}</h1>
-                </div><div class="img"><img src="${post.foto_post}" alt=""></div><div class="conteudo"><p>${post.descricao_post}</p></div></div>`;
-            
-        });
-    
+     const arraySort= data.sort(() => Math.random() - 0.5);
 
-        console.log(data);
-    } catch (error) {
-        console.error('Erro:', error);
+     const dataArray = arraySort.slice(0, 3);
+
+     
+     console.log(dataArray);
+     const cardBody = document.getElementById('row-card');
+ 
+     cardBody.innerHTML = '';
+     dataArray.forEach(materia => {
+     cardBody.innerHTML +=`
+           
+    <div class="card" style="width: 18rem;">
+    <img src="${materia.foto_post}" class="card-img-top" alt="...">
+    <div id="card-body" class="card-body">
+    <h5 class="card-title">${materia.titulo_post}</h5>
+    <a href="#" class="btn btn-primary">Ir para a materia</a>
+    </div>
+    </div>
+    </div>
+           
+    `
+    });
+    }catch(error){
+         console.error('Erro ao inserir materias', error);
     }
-    
-}
-async function innerCard(){
-    const api = await fetch(`${BACKEND}/posts`)
-
-    const data = await api.json();
-    console.log(data);
-}
-
-innerCard();
-
-async function postCategoria(){
-    const nome = document.getElementById('nome_categoria').value;
-    const descricao = document.getElementById('descricao').value;
-
-    const dados ={
-        nome_categoria: nome,
-        descricao_categoria: descricao
-    }
-
-        try {
-            const response = await fetch('http://localhost/RevistaDigital_API/categorias', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(dados)
-            });
-
-            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-success">Categoria criada com sucesso!</div>`;
-            document.getElementById('form-categoria').reset(); 
-        } catch (error) {
-            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-danger">Erro ao criar categoria: ${error.message}</div>`;
-        }
-}
-
-async function postMateria(){
-    const titulo_post = document.getElementById('titulo_post').value;
-    const foto_post = document.getElementById('foto_post').value;
-    const descricao_post = document.getElementById('descricao_post').value;
-    const data_criacao_post = document.getElementById('data_criacao_post').value;
-    const usuarios_id_usuario = document.getElementById('usuarios_id_usuario').value;
-    const categorias_id_categoria = document.getElementById('categorias_id_categoria').value;
-
-
-    const dados ={
-        titulo_post: titulo_post,
-        foto_post: foto_post,
-        descricao_post: descricao_post,
-        data_criacao_post: data_criacao_post,
-        usuarios_id_usuario: usuarios_id_usuario,
-        categorias_id_categoria: categorias_id_categoria
-    }
-
-        try {
-            const response = await fetch('http://localhost/RevistaDigital_API/posts', {
-                method: 'POST',
-                headers:{'Content-Type': 'application/json',},
-                body: JSON.stringify(dados)
-        });
-
-            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-success">Categoria criada com sucesso!</div>`;
-            document.getElementById('form-categoria').reset(); // Limpa o formulário
-        } catch (error) {
-            // Exibe mensagem de erro
-            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-danger">Erro ao criar categoria: ${error.message}</div>`;
-        }
-}
-
-
-
-async function postUsers(){
-    const user_usuario = document.getElementById('user_usuario').value;
-    const senha_usuario = document.getElementById('senha_usuario').value;
-    const nivel = document.getElementById('nivel').value;
-
-
-
-    const dados ={
-        user_usuario: user_usuario,
-        senha_usuario: senha_usuario,
-        nivel: nivel,
-       
-    }
-
-        try {
-            const response = await fetch('http://localhost/RevistaDigital_API/users', {
-                method: 'POST',
-                headers:{'Content-Type': 'application/json',},
-                body: JSON.stringify(dados)
-        });
-
-            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-success">Categoria criada com sucesso!</div>`;
-            document.getElementById('form-categoria').reset(); // Limpa o formulário
-        } catch (error) {
-            // Exibe mensagem de erro
-            document.getElementById('status_categoria').innerHTML = `<div class="alert alert-danger">Erro ao criar categoria: ${error.message}</div>`;
-        }
-}
+     
+ }
+ 
+ inerMateria();
